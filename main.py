2ec926b02a35e359c96b7060a0ec498721119fde
Руслан Мельник <melnk300@gmail.com>
2023-11-12 23:26:00 +0500

    IDK

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from auth.auth import AuthService
 
 
 def serve():
+    print("i'm alive")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     auth_pb2_grpc.add_AuthServiceServicer_to_server(AuthService(), server)
     server.add_insecure_port('[::]:50051')
--- utils/jwt.py
+++ utils/jwt.py
@@ -11,6 +11,7 @@ def generate_token(user_id):
         'id': data,
         'exp': datetime.utcnow() + exp,
     }
+
     return [
         jwt.encode(payload(user_id, timedelta(seconds=120)), os.getenv('JWT_SECRET'), algorithm='HS256'),
         jwt.encode(payload(user_id, timedelta(days=10)), os.getenv('JWT_SECRET'), algorithm='HS256'),
@@ -18,11 +19,4 @@ def generate_token(user_id):
 
 
 def decode_token(token):
-    try:
-        return jwt.decode(token, 'secret', algorithms=['HS256'])
-    except jwt.ExpiredSignatureError:
-        raise "Token expired"
-    except jwt.DecodeError:
-        raise "Token invalid"
-    except jwt.InvalidTokenError:
-        raise "Token invalid"
+    return jwt.decode(token, os.getenv('JWT_SECRET'), algorithms=['HS256'])
